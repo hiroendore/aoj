cfbbe4225e08d095ac53051cd9d9b517783f492b
遠藤 寛之 <hiro7592@gmail.com>
2017-10-29 17:01:15 +0900

    ITP1_6_A

--- aoj-itp1.cpp
+++ aoj-itp1.cpp
@@ -1,3 +1,49 @@
+// 2017/10/29
+
+//ITP1_6_A
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	int a[100];
+	for (int i=0;i<n;i++){
+		cin >> a[i];
+	}
+	
+	for (int i=0; i<n ; i++){
+		if (i!=0){cout << " ";}
+		cout << a[n-i-1];
+	}
+	cout << endl;
+	return 0;
+}
+
+
+#inlude<iostream>
+#define PI 3.141592653589
+using namespace std;
+ 
+int main(){
+    int n;
+    cin >>n;
+    int a[100];
+    for ( int i = 0; i < n; i++ ){
+        cin>>a[i];
+    }
+ 
+     
+    for ( int i = 0; i < n; i++ ){
+        if (i!=0){cout << " ";}
+        cout << a[n-i-1];
+    }
+    cout << endl;
+     
+    return 0;
+}
+
+
 // 2017/10/15
 
 #include<iostream>
